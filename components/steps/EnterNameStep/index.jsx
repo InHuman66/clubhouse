fdca64f298465b641dbf21d4d3d4dd702ff2eb91
InHuman66 <olexandr.kk@gmail.com>
2021-06-20 22:51:08 +0300

    add stepInfo component

--- components/steps/EnterNameStep/index.jsx
+++ components/steps/EnterNameStep/index.jsx
@@ -5,21 +5,9 @@ import { StepInfo } from '../../StepInfo';
 
 import styles from './EnterNameStep.module.scss';
 import React from 'react';
-import { MainContext } from '../../../pages';
 
 export const EnterNameStep = () => {
-  const [inputValue, setInputValue] = React.useState<string>('');
-  const { onNextStep } = React.useContext(MainContext);
 
-  const nextDisabled = !inputValue;
-
-  // const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setInputValue(event.target.value);
-  // };
-
-  const onClickNextStep = () => {
-    onNextStep();
-  };
 
   return (
     <div className={styles.block}>
@@ -31,15 +19,13 @@ export const EnterNameStep = () => {
       <WhiteBlock className={clsx('m-auto', styles.whiteBlock)}>
         <div className="mb-30">
           <input
-            onChange={handleChangeInput}
-            value={inputValue}
             className="field"
             placeholder="Enter fullname"
           />
         </div>
-        <Button disabled={nextDisabled} onClick={onClickNextStep}>
-          Next
-          <img className="d-ib ml-10" src="/static/arrow.svg" />
+        <Button>
+          {/*Next*/}
+          {/*<img className="d-ib ml-10" src="/static/arrow.svg" />*/}
         </Button>
       </WhiteBlock>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
-import {WelcomeStep} from "../components/steps/WelcomeStep";
-import React from "react";
+import {WelcomeStep} from "../components/steps/WelcomeStep/index";
+import React, {useState} from "react";
+import {EnterNameStep} from "../components/steps/EnterNameStep/index";
+
+const stepsComponents = {
+    0: WelcomeStep,
+    1: EnterNameStep,
+}
 
 
 export default function Home() {
-  const  [step, setStep] = React.useState<number>(1);
+    const [step, setStep]= useState<number>(0)
+    const Step = stepsComponents[step]
   return (
     <div>
-        <WelcomeStep/>
+        <Step/>
     </div>
   )
 }
